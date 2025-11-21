c1ebf157ffffdcef5e130f43656368bb9190707a
Adarsh maurya <mauryaadarsh793@gmail.com>
2025-11-21 15:03:33 +0530

    fix: cors

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
 import Jobs from "./models/jobs.model.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("dist"));
